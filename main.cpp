9080872b6fa81b5374bd533ea80fff377552b8f8
Filougoni <116490094+Filougoni@users.noreply.github.com>
2022-11-24 20:40:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,20 @@
 using namespace std;
 
 int main() {
-       int n=0;
-       int a=1, b=1,c=1;
+      int n=0,a=1,b=1,c=1;
        cin>>n;
        if(n>=2){
-           cout << a << endl;
-           cout << b << endl;
+           cout << a << endl<< b << endl;
        for (int i = 2; i < n; i++) {
-           c = a + b;
-           a = b;
-           (b = c);
+           c=a+b;
+           a=b;
+           b=c;
            cout << c << endl;
        }
        }
        else{
            cout << "errore"<<endl;
-           
+
        }
    return 0;
 }
