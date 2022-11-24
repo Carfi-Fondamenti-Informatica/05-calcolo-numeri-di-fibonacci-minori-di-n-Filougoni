941c03b07a9e726dac68601ba0adece6f153bab4
Filougoni <116490094+Filougoni@users.noreply.github.com>
2022-11-24 19:43:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int n, a=1,b=1, c;
+       cin >> n;
+       if (n<2) {
+           cout << "errore" << endl;
+       }else {
+           cout << a << endl;
+           cout << b << endl;
+           for (int i = 2; i < n; i++) {
+               c= a+b;
+               a=b;
+               b=c;
+               cout<< c <<endl;
+           }
+       }
    return 0;
 }
